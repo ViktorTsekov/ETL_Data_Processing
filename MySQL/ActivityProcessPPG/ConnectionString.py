2bf4dc9132cebfd79d6cf48419d4acd40be3b813
Viktor Tsekov <viktor@kymira.co.uk>
2021-04-15 16:27:59 +0100

    commit

---
+++ MySQL/ActivityProcessPPG/ConnectionString.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+def getDatabaseConnection() -> mysql.connector:
+
+    db = mysql.connector.connect(
+        host = "test-mysql-viktor.mysql.database.azure.com",
+        user = "Viktor@test-mysql-viktor",
+        passwd = "121233Vv",
+        database = "test_schema"
+    )
+
+    return db
