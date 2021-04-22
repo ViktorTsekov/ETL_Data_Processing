3e9b7056cd2950e09897fa35652008a1a291747e
Viktor Tsekov <viktor@kymira.co.uk>
2021-04-22 13:28:55 +0100

    commit

---
+++ TestAPI/TestAPI.py
@@ -0,0 +1,45 @@
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+
+CONNECTION_STR = "Endpoint=sb://kymira-servicebus-test.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ue1/RTffDJRWKUJITE5kHy8ik7MvlbGM/WkJcr59E0U="
+TOPIC_NAME = "testtopic"
+
+def send_single_message(sender):
+    message = ServiceBusMessage("Single Message")
+    sender.send_messages(message)
+    print("Sent a single message")
+
+def send_a_list_of_messages(sender):
+    messages = [ServiceBusMessage("Message in list") for _ in range(5)]
+    sender.send_messages(messages)
+    print("Sent a list of 5 messages")
+
+def send_batch_message(sender):
+    batch_message = sender.create_message_batch()
+    for _ in range(10):
+        try:
+            batch_message.add_message(ServiceBusMessage("Message inside a ServiceBusMessageBatch"))
+        except ValueError:
+            # ServiceBusMessageBatch object reaches max_size.
+            # New ServiceBusMessageBatch object can be created here to send more data.
+            break
+    sender.send_messages(batch_message)
+    print("Sent a batch of 10 messages")
+
+
+servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=True)
+
+with servicebus_client:
+    sender = servicebus_client.get_topic_sender(topic_name=TOPIC_NAME)
+    with sender:
+        send_single_message(sender)
+
+print("Done sending messages")
+print("-----------------------")
+"""
+with servicebus_client:
+    receiver = servicebus_client.get_queue_receiver(topic_name=TOPIC_NAME, max_wait_time=5)
+    with receiver:
+        for msg in receiver:
+            print("Received: " + str(msg))
+            receiver.complete_message(msg)
+"""
